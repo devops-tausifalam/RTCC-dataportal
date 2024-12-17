95d559cb378db37ac1cc9f8bb637490f40e09943
devops-tausifalam <83172943+devops-tausifalam@users.noreply.github.com>
2024-12-17 14:43:01 +0530

    gridJS

--- v7/dist/app.js
+++
@@ -1,9 +0,0 @@
-// handling ui
-let forwardtoModal = document.getElementById("forwardtoModal");
-let iConsent = document.getElementById("consent");
-forwardtoModal.addEventListener("click", (e) => {
-    e.preventDefault();
-    // perform form validation before consent modal
-    
-    iConsent.style.display = "block";
-});
